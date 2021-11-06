1a041ae7442a3e5bf938c55fefc0834c24a224e1
forrestyuan <2316539148@qq.com>
2021-11-06 23:52:32 +0800

    添加做题笔记

--- code/leetcode/验证二叉搜索树.js
+++ code/leetcode/验证二叉搜索树.js
@@ -1,5 +1,15 @@
-console.log('开始运行');
-//---------------------------------------------------------
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
 var isValidBST = function (root) {
   return loopTree(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
 };
